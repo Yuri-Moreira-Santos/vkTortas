75723dc86c5f84f594fadc15256da447c14bf75a
Yuri-Moreira-Santos <yurims.mw@gmail.com>
2026-05-14 15:37:54 -0300

    fix: dados iniciais

--- src/data/initialData.ts
+++ src/data/initialData.ts
@@ -14,7 +14,7 @@ export const INITIAL_INGREDIENTS: Ingredient[] = [
   { id: 'frango', name: 'Frango', unit: 'g' },
   { id: 'molho-tomate', name: 'Molho de Tomate', unit: 'g' },
   { id: 'azeitona', name: 'Azeitona', unit: 'g' },
-  { id: 'cebola', name: 'Cebola', unit: 'un' },
+  { id: 'cebola', name: 'Cebola', unit: 'g' },
   { id: 'alho', name: 'Alho', unit: 'un' },
   { id: 'milho', name: 'Milho', unit: 'g' },
   { id: 'paprica', name: 'Páprica Defumada', unit: 'g' },
@@ -41,7 +41,7 @@ export const INITIAL_PURCHASES: Purchase[] = [
   { id: 'p-frango', ingredientId: 'frango', date: INITIAL_DATE, price: 11.99, packageQuantity: 1000 },
   { id: 'p-molho', ingredientId: 'molho-tomate', date: INITIAL_DATE, price: 4.00, packageQuantity: 340 },
   { id: 'p-azeitona', ingredientId: 'azeitona', date: INITIAL_DATE, price: 5.99, packageQuantity: 185 },
-  { id: 'p-cebola', ingredientId: 'cebola', date: INITIAL_DATE, price: 2.99, packageQuantity: 4 },
+  { id: 'p-cebola', ingredientId: 'cebola', date: INITIAL_DATE, price: 2.99, packageQuantity: 750 },
   { id: 'p-alho', ingredientId: 'alho', date: INITIAL_DATE, price: 3.00, packageQuantity: 15 },
   { id: 'p-milho', ingredientId: 'milho', date: INITIAL_DATE, price: 2.59, packageQuantity: 285 },
   { id: 'p-paprica', ingredientId: 'paprica', date: INITIAL_DATE, price: 10.00, packageQuantity: 200 },
@@ -49,7 +49,7 @@ export const INITIAL_PURCHASES: Purchase[] = [
   { id: 'p-tempero', ingredientId: 'tempero-pronto', date: INITIAL_DATE, price: 6.15, packageQuantity: 136 },
   { id: 'p-pimenta', ingredientId: 'pimenta-reino', date: INITIAL_DATE, price: 4.00, packageQuantity: 40 },
   { id: 'p-cheiro-verde', ingredientId: 'cheiro-verde', date: INITIAL_DATE, price: 5.39, packageQuantity: 60 },
-  { id: 'p-calabresa', ingredientId: 'calabresa', date: INITIAL_DATE, price: 24.22, packageQuantity: 750 },
+  { id: 'p-calabresa', ingredientId: 'calabresa', date: INITIAL_DATE, price: 24.22, packageQuantity: 800 },
 ];
 
 export const INITIAL_RECIPES: Recipe[] = [
@@ -73,12 +73,12 @@ export const INITIAL_RECIPES: Recipe[] = [
   {
     id: 'recheio-frango',
     name: 'Recheio de Frango',
-    yields: 1,
+    yields: 33,
     ingredients: [
-      { ingredientId: 'frango', amount: 60 },
+      { ingredientId: 'frango', amount: 1500 },
       { ingredientId: 'molho-tomate', amount: 200 },
       { ingredientId: 'azeitona', amount: 150 },
-      { ingredientId: 'cebola', amount: 1 },
+      { ingredientId: 'cebola', amount: 40 },
       { ingredientId: 'alho', amount: 4 },
       { ingredientId: 'milho', amount: 170 },
       { ingredientId: 'paprica', amount: 10 },
@@ -92,12 +92,12 @@ export const INITIAL_RECIPES: Recipe[] = [
   {
     id: 'recheio-frango-catupiry',
     name: 'Recheio de Frango c/ Catupiry',
-    yields: 1,
+    yields: 33,
     ingredients: [
-      { ingredientId: 'frango', amount: 60 },
+      { ingredientId: 'frango', amount: 1500 },
       { ingredientId: 'molho-tomate', amount: 200 },
       { ingredientId: 'azeitona', amount: 150 },
-      { ingredientId: 'cebola', amount: 1 },
+      { ingredientId: 'cebola', amount: 40 },
       { ingredientId: 'alho', amount: 4 },
       { ingredientId: 'milho', amount: 170 },
       { ingredientId: 'paprica', amount: 10 },
@@ -112,20 +112,11 @@ export const INITIAL_RECIPES: Recipe[] = [
   {
     id: 'recheio-calabresa',
     name: 'Recheio de Calabresa',
-    yields: 1,
+    yields: 25,
     ingredients: [
-      { ingredientId: 'calabresa', amount: 60 },
-      { ingredientId: 'molho-tomate', amount: 200 },
-      { ingredientId: 'azeitona', amount: 150 },
-      { ingredientId: 'cebola', amount: 1 },
-      { ingredientId: 'alho', amount: 4 },
-      { ingredientId: 'milho', amount: 170 },
-      { ingredientId: 'paprica', amount: 10 },
-      { ingredientId: 'sazon', amount: 5 },
-      { ingredientId: 'tempero-pronto', amount: 5 },
-      { ingredientId: 'pimenta-reino', amount: 5 },
-      { ingredientId: 'cheiro-verde', amount: 15 },
-      { ingredientId: 'sal', amount: 5 },
+      { ingredientId: 'calabresa', amount: 800 },
+      { ingredientId: 'cebola', amount: 40 },
+      { ingredientId: 'cheiro-verde', amount: 30 },
     ],
   },
 ];
@@ -159,4 +150,4 @@ export const INITIAL_SETTINGS: Settings = {
   },
 };
 
-export const DATA_VERSION = '1.6';
+export const DATA_VERSION = '1.0';
